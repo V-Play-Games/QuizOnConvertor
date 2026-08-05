afe6abd4a5cdd8b3d8ea08658fe2d05616384c74
V Play Games <vaibhavnargwani28@gmail.com>
2026-08-05 09:36:34 +0530

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+}
+
+group = "net.vplaygames"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
